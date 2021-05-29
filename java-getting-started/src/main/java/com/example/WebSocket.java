b9e1afc1db5d945bc807df9aec7d1cc84676bb4a
Anand Swaroop <pritirekha@gmail.com>
2021-05-29 15:50:13 -0400

    Modified WebSocket.java

--- java-getting-started/src/main/java/com/example/WebSocket.java
+++ java-getting-started/src/main/java/com/example/WebSocket.java
@@ -1,56 +1,42 @@
 package com.example;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-public class WebSocket {
-    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-        ServerSocket server = new ServerSocket(80);
-        try {
-            System.out.println("Server has started on 127.0.0.1:80.\r\nWaiting for a connection...");
-            Socket client = server.accept();
-            System.out.println("A client connected.");
+public class WebSocket extends TextWebSocketHandler {
 
-            InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
-            Scanner s = new Scanner(in, "UTF-8");
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocket.class);
 
+    private final List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        sessions.remove(session);
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        sessions.forEach(webSocketSession -> {
             try {
-                String data = s.useDelimiter("\\r\\n\\r\\n").next();
-                Matcher get = Pattern.compile("^GET").matcher(data);
-
-                if (get.find()) {
-                    Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                    match.find();
-                    byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
-                            + "Connection: Upgrade\r\n"
-                            + "Upgrade: websocket\r\n"
-                            + "Sec-WebSocket-Accept: "
-                            + Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8")))
-                            + "\r\n\r\n").getBytes("UTF-8");
-                    out.write(response, 0, response.length);
-
-                    byte[] decoded = new byte[6];
-                    byte[] encoded = new byte[] { (byte) 198, (byte) 131, (byte) 130, (byte) 182, (byte) 194, (byte) 135 };
-                    byte[] key = new byte[] { (byte) 167, (byte) 225, (byte) 225, (byte) 210 };
-                    for (int i = 0; i < encoded.length; i++) {
-                        decoded[i] = (byte) (encoded[i] ^ key[i & 0x3]);
-                    }
-                }
-            } finally {
-                s.close();
+                webSocketSession.sendMessage(message);
+            } catch (IOException e) {
+                LOGGER.error("Error occurred.", e);
             }
-        } finally {
-            server.close();
-        }
+        });
     }
 }
